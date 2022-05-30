8ebe78d21add1fa3a4a91f6e33775ac1a4080b0a
AllMightyWeak <allmightyweak@gmail.com>
2022-05-30 09:15:47 +0300

    Ver 1.0

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,4 +1,4 @@
-//Copyright by Enigma
+// Copyright by AllMightyWeak
 
 #include <boost/program_options.hpp>
 #include <example.hpp>
--- include/example.hpp
+++ include/example.hpp
@@ -1,4 +1,4 @@
-// Copyright by Enigma
+// Copyright by AllMightyWeak
 
 #ifndef INCLUDE_EXAMPLE_HPP_
 #define INCLUDE_EXAMPLE_HPP_
